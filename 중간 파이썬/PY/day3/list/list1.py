3f968c5d0da7c13da3791e1d1140742879e64aef
lsj0202 <w1004123@gmail.com>
2022-10-06 21:46:26 +0900

    day 3

---
+++ 중간 파이썬/PY/day3/list/list1.py
@@ -0,0 +1,13 @@
+my_list =['오예스','몽쉘','초코파이'] #리스트 추가,삭제 하기 가능
+
+my_list.append('빅파이')
+print(my_list)
+#=>오예스,몽쉘,초코파이,빅파이
+
+my_list.remove('빅파이')
+print(my_list)
+#=>오예스,몽쉘,초코파이
+
+my_list.append('핫식스')
+print(my_list)
+#=>오예스,몽쉘,초코파이,핫식스
---
+++ 중간 파이썬/PY/day3/list/list2.py
@@ -0,0 +1,8 @@
+tot_list=['아','이','스']
+sum_list=['크','림']
+
+sum_list.extend(tot_list) #리스트 확장
+print(sum_list)
+
+tot_list.extend(sum_list) #리스트 확장
+print(tot_list)
