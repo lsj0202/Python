f390751370293c27dc77a39b4a74cb517efdf596
BSSM 2기 <102288397+lsj0202@users.noreply.github.com>
2022-10-11 22:30:59 +0900

    Day 7 + 복습

---
+++ Python/중간 파이썬/PY/Day 7/ex1.py
@@ -0,0 +1,28 @@
+# # # -*- coding: utf-8 -*-
+# # # UTF-8 encoding when using korean
+# # # gun = 10
+# # # def checkpoint(soldier):
+# # # 	global gun #전역변수를 쓰려면 global을 붙여야함 ///그래야 오류 x
+# # # 	gun = gun - soldier
+# # # 	print(f"[함수 내] 남은 총 :{gun}")
+# #
+# # # print(f"전체 총: {gun}")
+# # # checkpoint(2)
+# # # print(f"남은총: {gun}")
+# #
+# # gun = 10
+# #
+# #
+# # def checkpoint_ret(gun, soldier):
+# #     gun = gun - soldier
+# #     print(f"[함수 내] 남은 총: {gun}")
+# #     return gun
+# #
+# #
+# # print(f"전체 총: {gun}");
+# #
+# # gun = checkpoint_ret(gun, 2)
+# #
+# # print(f"남은 총: {gun}")
+# def sec():
+#     mss =12312312
